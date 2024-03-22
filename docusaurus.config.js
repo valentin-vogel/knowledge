e0c34afac38316e8d74447dc192ecdc8ed7561c5
Valentin Vogel <vogelvali@gmail.com>
2024-03-22 12:39:02 +0000

    fix: folder structure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   url: process.env.URL || 'https://valentin-vogel.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL || '/knowledge/',
+  baseUrl: process.env.BASE_URL || '/knowledge',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -36,6 +36,7 @@ const config = {
         docs: {
           sidebarPath: './sidebars.js',
           routeBasePath: '/',
+          path: 'knowledge',
         },
         blog: {
           showReadingTime: true,
